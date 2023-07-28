d5b386ae8f199ec2133bc5f9676e1d440ac91f5e
Gunesh <gunesh.salyhovvv@gmail.com>
2023-07-28 10:22:46 +0500

    commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Cookie_Authentication_With_ASP.NET_Core_6_Web_api;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
